625e3e59ef93b611a99999a6874b6939794ea201
William Strecker-Kellogg <willsk@bnl.gov>
2020-03-04 10:39:38 -0500

    Add ability to override path per-account

--- sdccslurm.py
+++ sdccslurm.py
@@ -1,3 +1,6 @@
+import os
+import re
+
 from traitlets import Unicode
 from tornado.log import app_log
 
@@ -13,23 +16,35 @@ class SDCCSlurmSpawner(FormMixin, SlurmSpawner):
     req_runtime = Unicode('30:00')
     req_gputype = Unicode('')
     req_scontainer = Unicode('')
+    req_kpath = Unicode('/u0b/software/jupyter/')
+
+    def check_path_override(self, account):
+        kern_cfgfile = 'conf/kerneloverride.cfg'
+        path = os.path.join(os.path.dirname(__file__), kern_cfgfile)
+        with open(path) as fp:
+            for regex, jp in (x.split() for x in fp):
+                if re.match(regex, account):
+                    self.log.info("Matched %s to %s, using jupyter_path = %s",
+                                  account, regex, jp)
+                    self.req_kpath = jp
 
     @property
     def batch_script(self):
-        # self.log.info("My form Options: %s", self.user_)
         self.log.info("My Actual Options: %s", self.user_options)
         base = [('partition', '{partition}'), ('account', '{account}'),
                 ('time', '{runtime}')]
-        if 'req_cpus' in self.user_options:
+        if 'cpus' in self.user_options:
             base += [('cpus-per-task', '{nprocs}')]
-        if 'req_memory' in self.user_options:
+        if 'memory' in self.user_options:
             base += [('mem', '{memory}G')]
-        if 'req_ngpus' in self.user_options:
+        if 'ngpus' in self.user_options:
             base += [('gres', 'gpu:{ngpus}')]
-        if 'req_gputype' in self.user_options:
+        if 'gputype' in self.user_options:
             base += [('constraint', '{gputype}')]
-        if 'req_qos' in self.user_options:
+        if 'qos' in self.user_options:
             base += [('qos', '{qos}')]
+        self.check_path_override(self.user_options.get('account'))
+
         prescript = '\n'.join('#SBATCH --{key}={value}'.format(
                                 key=x[0], value=x[1]) for x in base)
         return '#!/bin/sh\n' + prescript + '''
@@ -41,6 +56,7 @@ class SDCCSlurmSpawner(FormMixin, SlurmSpawner):
 #SBATCH {options}
 export PATH="{jhubpath}:$PATH"
 export SCONTAINER="{scontainer}"
+export JUPYTER_PATH="''' + self.req_kpath + '''"
 unset XDG_RUNTIME_DIR
 module load cuda/9.0
 {cmd}
